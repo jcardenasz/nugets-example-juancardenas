a7532ae5f28f2972202353f8a059c3b4d9754362
Juan Cárdenas Zabala <juan.cardenas@endava.com>
2025-09-15 17:10:09 -0500

    base complete example

---
+++ nugets-and-tools/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string jsonText = @"{
+    'Make': 'Toyota',
+    'Model': 'Camry',
+    'Year': 2020
+}";
+
+Car car = JsonConvert.DeserializeObject<Car>(jsonText);
+
+Console.WriteLine($"Car Make: {car.Make}, Model: {car.Model}, Year: {car.Year}");
+
+public class Car
+{
+    public required string Make { get; set; }
+    public required string Model { get; set; }
+    public required int Year { get; set; }
+}
---
+++ nugets-second-part/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var Lada = new Car(){
+    Make = "Lada",
+    Model = "Niva",
+    Year = 2024
+};
+
+var jsonText = JsonConvert.SerializeObject(Lada);
+Console.WriteLine(jsonText);
+
+public class Car
+{
+    public required string Make { get; set; }
+    public required string Model { get; set; }
+    public required int Year { get; set; }
+}
